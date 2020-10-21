eef6972bc9e359db02c768c9bb7c61e8bb49620c
IvonaZoricic <ivona.zoricic@gmail.com>
2020-10-21 15:51:03 +0200

    Added metods in BensinCounter

--- src/BensinCounter.java
+++ src/BensinCounter.java
@@ -1,3 +1,10 @@
 public class BensinCounter {
 
+    public double countedMileage(double amountOfGasThen, double amountOfGasNow){
+        return amountOfGasNow-amountOfGasThen;
+    }
+
+    public double usagePerMil(double averageGasUsage, double milPerYear){
+        return averageGasUsage/milPerYear;
+    }
 }
